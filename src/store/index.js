8cd0ef2b5a55b4f37d40b2b19748f6f5b5870fca
Kay Pfeuffer <k.pfeuffer@ipi-gmbh.com>
2019-06-11 06:13:07 +0200

    Fixed Store-Action Name

--- src/store/index.js
+++ src/store/index.js
@@ -10,11 +10,16 @@ const initialState = {
 export const reducer = (state, action)=> {
     switch(action.type) {
         case Actions.initialize: {
+            console.log("state initialized");
             return initialState;
         }break;
-        case Actions.createWorkoutsheet: {
+        case Actions.createWorkoutSheet: {
             console.log(state, action);
         }break;
+        default: {
+            console.log('store defaulted', action);
+            return state;
+        }
     }
     return state;
 
